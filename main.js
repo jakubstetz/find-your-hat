285127885978abb78d644270e4224a2c8d02693c
Hugo <119013233+hugo-ste@users.noreply.github.com>
2024-09-16 16:33:31 -0400

    Complete Task 3: Adding print method to Field class

--- main.js
+++ main.js
@@ -9,6 +9,18 @@ const characters = {
 
 class Field {
   constructor(input) { // input represents the field.
+    this.field = input;
+  }
 
+  print() { // Print the field.
+    for (const row of this.field) console.log(row.join(''));
   }
 }
+
+const myField = new Field([
+  ['*', '░', 'O'],
+  ['░', 'O', '░'],
+  ['░', '^', '░'],
+]);
+
+myField.print();
